b5dba87c9d2a6e06adc5134819e6d14cabde41bb
ra-raptor <vikasvks0112@gmail.com>
2023-07-06 10:05:41 +0530

    cors

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ dotenv.config()
 const port = 5000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({credentials: true, 
+    origin: 'http://localhost:3000', 
+    methods:['GET','POST','PUT','DELETE','OPTIONS']
+}))
 
 const catHelper = async (all_cat) => {
     const catList = []
