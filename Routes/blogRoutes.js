d8cd5bc75ba7d7f4a1b91013fa978fc86366ecd8
ra-raptor <vikasvks0112@gmail.com>
2023-07-06 10:16:13 +0530

    cors3

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -5,7 +5,18 @@ import { createBlog,deleteBlog,updateBlog, viewBlog,blogList, likeBlog,visitBlog
 const router = Router()
 
 router.use(cookieParser())
-
+router.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000',];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 
 router.route('/').post(checkAuth, createBlog).get(blogList)
 
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,6 +6,18 @@ const router = Router();
 
 router.use(express.json());
 router.use(cookieParser());
+router.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000',];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 
 router.get("/logout", checkAuth, userLogout)
 
