78739d393019411159ec556372db858cd636cde3
ra-raptor <vikasvks0112@gmail.com>
2023-07-09 10:46:52 +0530

    handled error

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -1,21 +1,30 @@
 import { Router } from "express";
 import checkAuth from "../Middlewares/checkAuth.js";
 import cookieParser from "cookie-parser";
-import { createBlog,deleteBlog,updateBlog, viewBlog,blogList, likeBlog,visitBlog } from "../Controllers/blogController.js";
-const router = Router()
-import cors from 'cors'
-router.use(cookieParser())
-router.use(cors({
-    origin: ['https://shayrana.netlify.app', 'http://localhost:3000'],
-    credentials: true
-  }));
+import {
+	createBlog,
+	deleteBlog,
+	updateBlog,
+	viewBlog,
+	blogList,
+	likeBlog,
+	visitBlog,
+} from "../Controllers/blogController.js";
+const router = Router();
+import cors from "cors";
+router.use(cookieParser());
+router.use(
+	cors({
+		origin: ["https://shayrana.netlify.app", "http://localhost:3000"],
+		credentials: true,
+	})
+);
 
-router.route('/').post(checkAuth, createBlog).get(blogList)
+router.route("/").post(createBlog).get(blogList);
 
-router.put('/like/:id', checkAuth, likeBlog)
-router.put('/view/:id', visitBlog)
+router.put("/like/:id", checkAuth, likeBlog);
+router.put("/view/:id", visitBlog);
 
-router.route('/:id').get(viewBlog).put( updateBlog).delete( deleteBlog)
-
+router.route("/:id").get(viewBlog).put(updateBlog).delete(deleteBlog);
 
 export default router;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,30 +1,37 @@
 import express, { Router } from "express";
 import cookieParser from "cookie-parser";
-import {userDetails, userLogin, userLogout, userSignup} from "../Controllers/userController.js"
+import {
+	userDetails,
+	userLogin,
+	userLogout,
+	userSignup,
+} from "../Controllers/userController.js";
 import checkAuth from "../Middlewares/checkAuth.js";
 const router = Router();
-import cors from 'cors'
+import cors from "cors";
 router.use(express.json());
 router.use(cookieParser());
-router.use(cors({
-    origin: ['https://shayrana.netlify.app', 'http://localhost:3000'],
-    credentials: true
-  }));
+router.use(
+	cors({
+		origin: ["https://shayrana.netlify.app", "http://localhost:3000"],
+		credentials: true,
+	})
+);
 
-router.get("/logout", checkAuth, userLogout)
+router.get("/logout", userLogout);
 
-router.post("/login", userLogin)
+router.post("/login", userLogin);
 
-router.post("/signup", userSignup)
+router.post("/signup", userSignup);
 
-router.get("/me", checkAuth, (req,res) => {
-    const {_id} = req.currUser
-    res.json({
-        success : true,
-        _id
-    })
-})
+router.get("/me", checkAuth, (req, res) => {
+	const { _id } = req.currUser;
+	res.json({
+		success: true,
+		_id,
+	});
+});
 
-router.get("/:id", userDetails)
+router.get("/:id", userDetails);
 
 export default router;
